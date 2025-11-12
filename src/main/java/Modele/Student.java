fa2448a54fe8d47e4576bd3abfbba5edd41aeaaa
ElmehdiHachem <Elmehdihachem1@gmail.com>
2025-11-12 11:24:32 +0100

    commit

--- src/main/java/Modele/Student.java
+++ src/main/java/Modele/Student.java
@@ -1,4 +1,14 @@
 package Modele;
 
+import enums.Genre;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Student {
+    private int id;
+    private String nom;
+    private String prenom;
+    private Genre genre;
 }
